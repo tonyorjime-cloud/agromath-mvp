e9dcc30ee5536200a23f484812a8a484c730241f
Tony <tony.orjime@gmail.com>
2025-12-30 05:28:04 +0100

    Serve OneSignal service worker via Flask route

--- app.py
+++ app.py
@@ -1241,6 +1241,15 @@ def transport_deliver():
 
     flash("Order marked delivered.", "ok")
     return redirect(url_for("transporter_dashboard"))
+from flask import send_from_directory
+
+@app.route("/OneSignalSDKWorker.js")
+def onesignal_worker():
+    return send_from_directory(
+        directory="static/onesignal",
+        path="OneSignalSDKWorker.js",
+        mimetype="application/javascript"
+    )
 
 
 
